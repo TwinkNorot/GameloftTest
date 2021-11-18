fe4a728245c394e55cb3951bdd06e26ab7456acc
TwinkNorot <assie.louis@gmail.com>
2021-11-18 19:13:25 +0100

    Adding basic comportment for turret, basic bullet

---
+++ Gameloft_Test_LouisAssié/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public string damageType;
+    public float range;
+    public float damage;
+    public float armorPenetration;
+    public float projectileSpeed;
+    public Vector3 parentPosition;
+
+    public void Init(string DamageType, float Range, float Damage, float ArmorPenetration, float ProjectileSpeed, Vector3 ParentPosition)
+    {
+        damageType = DamageType;
+        range = Range;
+        damage = Damage;
+        armorPenetration = ArmorPenetration;
+        projectileSpeed = ProjectileSpeed;
+        parentPosition = ParentPosition;
+    }
+}
---
+++ Gameloft_Test_LouisAssié/Assets/Scripts/BulletControler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControler : MonoBehaviour
+{
+
+    Bullet stats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = GetComponent<Bullet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * stats.projectileSpeed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, stats.parentPosition) >= stats.range)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
